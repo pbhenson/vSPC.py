5515de9b4195e9996e6412b463008ae34801e50e
Kevan Carstensen <kacarstensen@csupomona.edu>
2011-12-07 13:40:00 -0800

    Add vSPCBackendLogging.py
    vSPCBackendLogging is a subclass of vSPCBackendMemory. It logs all
    messages received from VMs to logfiles associated with those VMs, and
    manages those log files.

---
+++ vSPCBackendLogging.py
@@ -0,0 +1,95 @@
+# Copyright 2011 California State Polytechnic University, Pomona. All Rights Reserved
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice, this list of
+#       conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list
+#       of conditions and the following disclaimer in the documentation and/or other materials
+#       provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY California State Polytechnic University, Pomona.
+# ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are those of the
+# authors and should not be interpreted as representing official policies, either expressed
+# or implied, of California State Polytechnic University, Pomona.
+
+import signal
+import logging
+import optparse
+import shlex
+
+__author__    = "Kevan Carstensen"
+__copyright__ = "Copyright (C) 2011 California State Polytechnic University, Pomona"
+
+from vSPC import vSPCBackendMemory
+
+class vSPCBackendLogging(vSPCBackendMemory):
+    """
+    I'm a backend for vSPC.py that logs VM messages to a file or files.
+    """
+    def setup(self, args):
+        parsed_args = self.parse_args(args)
+
+        self.logdir = parsed_args.logdir
+        self.prefix = parsed_args.prefix
+        # filename => filehandle
+        self.logfiles = {}
+
+        # register for SIGHUP, so we know when to reload logfiles.
+        signal.signal(signal.SIGHUP, self.handle_sighup)
+
+    def vm_msg_hook(self, uuid, name, msg):
+        f = self.file_for_vm(name, uuid)
+        try:
+            f.write(msg)
+        except ValueError, e:
+            # we tried to write to a closed fd, which means that we were
+            # told to reload our log files between when we got the file
+            # descriptor and when we tried to write to it. if we try
+            # again, it should go through, so just do that.
+            return self.vm_msg_hook(uuid, name, msg)
+
+    def parse_args(self, args):
+        # XXX: Annoying; it would be nicer if OptionParser would print
+        # out a more verbose message upon encountering unrecognized
+        # arguments
+        u = "%prog ...--backend-args='[ [ (-l | --logdir) logdir ] [ (-p | --prefix) prefix ] ]'"
+        parser = optparse.OptionParser(usage=u)
+        parser.add_option("-l", "--logdir", type='string',
+                          action='store', default="/var/log/consoles",
+                          help='Directory in which log files are written')
+        parser.add_option("-p", "--prefix", default='vmlog', type='string',
+                          help="First part of log file names")
+        args_list = shlex.split(args)
+        (options, args) = parser.parse_args(args_list)
+        return options
+
+    def file_for_vm(self, name, uuid):
+        if uuid not in self.logfiles:
+            filename = "%s/%s-%s" % (self.logdir, self.prefix, name)
+            self.logfiles[uuid] = open(filename, "a")
+        return self.logfiles[uuid]
+
+    def reload(self):
+        for k, f in self.logfiles.iteritems():
+            f.close()
+            self.logfiles[k] = open(k, "a")
+
+    def handle_sighup(self, signum, frame):
+        assert signum == signal.SIGHUP
+
+        logging.info('vspc received reload request, reopening log files')
+        self.reload()

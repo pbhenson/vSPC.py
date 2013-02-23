5f4719ce9053a0dace4f8e82ca7e1f226eac1359
Kevan Carstensen <kevan@isnotajoke.com>
2013-02-23 09:52:31 -0800

    Add general utility function list; seed w/ terminal manipulation helpers

---
+++ lib/util.py
@@ -0,0 +1,36 @@
+# lib/util.py -- utility functions & classes
+
+def prepare_terminal(fd):
+    """
+    Prepare a terminal for interactive operation. Take a file
+    descriptor-like object as an argument, return the original terminal
+    state (oldterm & oldflags)
+    """
+    oldterm = termios.tcgetattr(fd)
+    newattr = oldterm[:]
+    # this is essentially cfmakeraw
+
+    # input modes
+    newattr[0] = newattr[0] & ~(termios.IGNBRK | termios.BRKINT | \
+                                termios.PARMRK | termios.ISTRIP | \
+                                termios.IGNCR | termios.ICRNL | \
+                                termios.IXON)
+    # output modes
+    newattr[1] = newattr[1] & ~termios.OPOST
+    # local modes
+    newattr[3] = newattr[3] & ~(termios.ECHO | termios.ECHONL | \
+                                termios.ICANON | termios.IEXTEN | termios.ISIG)
+    # special characters
+    newattr[2] = newattr[2] & ~(termios.CSIZE | termios.PARENB)
+    newattr[2] = newattr[2] | termios.CS8
+
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    return (oldterm, oldflags)
+
+def restore_terminal(fd, oldterm, oldflags):
+    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)

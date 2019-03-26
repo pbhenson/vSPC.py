a045a1abddb82d1e891a3a6094b1ec6aa3fd47e2
Paul B. Henson <henson@acm.org>
2019-03-26 15:14:17 -0700

    Use a random 32 bit value here.

--- lib/telnet.py
+++ lib/telnet.py
@@ -348,7 +348,7 @@ class VMTelnetServer(TelnetServer):
                 "%s, uri: %s), will not proxy for this VM", dir, uri)
 
     def _handle_vmotion_begin(self, data):
-        cookie = data + struct.pack("I", hash(self) & 0xFFFFFFFF)
+        cookie = data + struct.pack("I", os.urandom(4))
 
         if self.handler.handle_vmotion_begin(self, cookie):
             logging.debug("vMotion initiated: %s", hexdump(cookie))

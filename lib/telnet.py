291beb532e73c71204c485d203e973d8493fed46
Paul B. Henson <henson@acm.org>
2019-03-26 12:40:07 -0700

    This needs to be unsigned.

--- lib/telnet.py
+++ lib/telnet.py
@@ -347,7 +347,7 @@ class VMTelnetServer(TelnetServer):
                 "%s, uri: %s), will not proxy for this VM", dir, uri)
 
     def _handle_vmotion_begin(self, data):
-        cookie = data + struct.pack("i", hash(self) & 0xFFFFFFFF)
+        cookie = data + struct.pack("I", hash(self) & 0xFFFFFFFF)
 
         if self.handler.handle_vmotion_begin(self, cookie):
             logging.debug("vMotion initiated: %s", hexdump(cookie))
